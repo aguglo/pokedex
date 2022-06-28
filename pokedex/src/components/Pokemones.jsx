d169820d4ade4be5d2b6fc8cecaf57b58ccc9113
iAGuss <gustavodario2001@gmail.com>
2022-06-28 14:02:08 -0300

    nose

--- pokedex/src/components/Pokemones.jsx
+++ pokedex/src/components/Pokemones.jsx
@@ -1,24 +1,36 @@
 import "./pokemones.css";
 import Pokemon from "./CartaP";
 import constantepokemons from "../constantes/listapokemones";
-import React, {useState} from "react";
-
+import React, { useState } from "react";
+import Header from "./Header";
 
 function Pokemones() {
+  const [pokemons, setpokemons] = useState(constantepokemons);
+  // const alfabeticamente =(nuevopokemons)=>{
+  //   return nuevopokemons.sort((a,z)=>
+  //     a.name-z.name)
+  //   }
+  // }
 
-    const [pokemons, setpokemons] = useState(constantepokemons);
-    
-  
   return (
     <main>
-      {<div className="container">
-        {pokemons.map((pokemon) => (
-          <Pokemon key={pokemon.name} pokemon={pokemon} />
-        ))}
+      {
+        <div className="container">
+          {pokemons.map((pokemon) => (
+            <Pokemon key={pokemon.name} pokemon={pokemon} />
+          ))}
+          {/* {
+          pokemons.sort((pokemonesOrdenados)=>(
+          <Header key={pokemons.name}pokemonesOrdenados={pokemonesOrdenados}/>
 
-       
-       
-      </div> }
+          )
+          
+          
+          
+          )
+        } */}
+        </div>
+      }
     </main>
   );
 }
