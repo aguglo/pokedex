ae49daab754b97cd008970e373c7dfa3450f3ad9
Agustina <aguglo@hotmail.com>
2022-06-29 11:14:14 -0300

    carta

--- pokedex/src/components/Infocontenedor.jsx
+++ pokedex/src/components/Infocontenedor.jsx
@@ -1,15 +1,45 @@
 import React from "react";
 import "./infocontenedor.css";
+import arrowleft from "../assets/arrow-left.svg";
 
 function Infocontenedor({ info }) {
   if (info) {
     const imagen = require(`../assets/${info.name.toLowerCase()}.png`);
     return (
       <div>
+        <header>
+
         <nav>
-          <h1>eu confio {info.name}</h1>
-          <img src={imagen} alt="" />
+
+          <div className="nav">
+            <img src={arrowleft} />
+          <h1>{info.name}</h1>          
+          </div>
+
+          <p>{info.number}</p>
+          
         </nav>
+
+        <img src={imagen} alt="" />
+        <p>></p>
+
+        </header>
+
+        
+
+        <main>
+
+        <div>
+            <span>{info.type1}</span>            
+            <span>{info.type2}</span>
+          </div>
+
+          <h3>About</h3>
+
+          
+
+        </main>
+
       </div>
     );
   }
